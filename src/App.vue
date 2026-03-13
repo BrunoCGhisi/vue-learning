2185a11aaec03ac1b48334a40577658f2790675a
brunocghisi <brunocostaghisi@gmail.com>
2026-03-13 17:06:19 -0300

    basics - first component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from 'vue';
 import ButtonCounter from '@/components/ButtonCounter.vue'
+import ButtonDuplicater from '@/components/ButtonCounter.vue'
+
 
 const counter = ref(0);
+const duplicated = ref(0)
 
 function plusOne(){ counter.value++; }
 function removeOne(){
@@ -15,6 +18,10 @@ function removeOne(){
 }
 function resetCounter(){ counter.value = 0; }
 
+function duplicate(){
+  duplicated.value = counter.value *2;
+}
+
 </script>
 
 <template>
@@ -23,17 +30,16 @@ function resetCounter(){ counter.value = 0; }
     <div class="header">
       <h1>Controller Buttons</h1>
       <p>The actual mountant of clicks is: <strong>{{counter}}</strong></p>
+      <p>The actual mountant of clicks is: <strong>{{duplicated}}</strong></p>
     </div>
 
     <div class="container">
-
-        <ButtonCounter text="Plus" color="#00ffff" @clicked="plusOne" />
-
-
-        <ButtonCounter text="Minus" color="#ff00ff" @clicked="removeOne" />
-
-        <ButtonCounter text="Reset" color="#ffcc00" @clicked="resetCounter" />
-
+      <ButtonCounter text="Plus" color="#00ffff" @clicked="plusOne" />
+      <ButtonCounter text="Minus" color="#ff00ff" @clicked="removeOne" />
+      <ButtonCounter text="Reset" color="#ffcc00" @clicked="resetCounter" />
+    </div>
+    <div class="container">
+      <ButtonDuplicater text="Plus" color="#000" @clicked="duplicate" />
     </div>
   </div>
 
@@ -56,8 +62,7 @@ body{
 .container {
   display: flex;
   flex-direction: row;
-
-  background: #e6adc0;
+  background: #ecccd9;
   gap: 20px;
   justify-content: center;
 }
