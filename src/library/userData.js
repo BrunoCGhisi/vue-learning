2cbe80f0cf7e881a79871c82bda1a4722dd4e4cc
brunocghisi <brunocostaghisi@gmail.com>
2026-03-30 14:18:25 -0300

    data change

--- src/library/userData.js
+++ src/library/userData.js
@@ -1,9 +1,10 @@
 import { User } from '@/models/User'
 
 export const initialUsers = [
-  new User(1, 'Veigar', 25, ['Vue', 'Node', 'Java', 'Kotlin', 'Typescript'], true),
-  new User(2, 'Omen', 15, ['HTML', 'CSS'], false),
-  new User(3, 'Cypher', 20, ['JavaScript', 'Python', 'Html', 'Css', 'WordPress'], true),
-  new User(4, 'DoorsMan', 26, ['Vue', 'Firebase'], true),
-  new User(5, 'Viscuos', 17, ['HTML', 'CSS', 'Design'], false),
+  new User(1, 'Veigar', 500, ['Farm', 'ik', '999 ap+', 'HihiHAHAH', 'The Pure Evil'], false),
+  new User(2, 'Omen', 28, ['Teleport', 'Shadows Manipulation', 'They will run', 'Paranoia'], true),
+  new User(3, 'Cypher', 32, ['Spy', 'Techs', 'Handsome', 'Information'], false),
+  new User(4, 'DoorsMan', 999999, ['Serving', 'Not', 'Rule.'], true),
+  new User(5, 'Viscuos', 1, ['Cube', 'Goo', 'Cube'], true),
+  new User(6, 'Dynamo', 1, ['Astronomia', 'Physics', 'Teaching'], true),
 ]
