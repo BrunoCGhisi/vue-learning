7e1ddc9f5adefd3fdbd6e6bd17b3c0b64b799ebc
brunocghisi <brunocostaghisi@gmail.com>
2026-04-02 14:47:01 -0300

    ibge search - v-if v-else logic working

--- src/components/IBGE/Ibge-section.vue
+++ src/components/IBGE/Ibge-section.vue
@@ -8,9 +8,9 @@ const searchTerm = ref('')
 const result = ref('')
 const searched = ref(false)
 
-function template() {
-  searched.value = false
+function searchIbge() {
   console.log(`Buscando por: "${searchTerm.value}" na URL: ${props.url + searchTerm.value}" `)
+  result.value = result.value + 1
   return (searched.value = true)
 }
 </script>
@@ -19,18 +19,23 @@ function template() {
   <div class="ibge-section">
     <div class="section-title">{{ title }}</div>
     <div>
-      <input v-model="searchTerm" type="text" :placeholder="placeholderTextfield" />
+      <input
+        @input="searched = false"
+        v-model="searchTerm"
+        type="text"
+        :placeholder="placeholderTextfield"
+      />
     </div>
     <div>
       <div>
-        <ButtonContrast text="Search" @clicked="template()" />
+        <ButtonContrast text="Search" @clicked="searchIbge()" />
       </div>
       <div>
         <p v-if="searched">
-          Searching the {{ title }} of: <strong>{{ searchTerm }}</strong>
+          Searching the {{ title }} of: <strong>{{ searchTerm }} has {{ result }} results</strong>
         </p>
         <p v-else-if="searchTerm">
-          Searching the {{ title }} of: <strong>{{ searchTerm }} has {{ result }} results</strong>
+          Searching the {{ title }} of: <strong>{{ searchTerm }}</strong>
         </p>
       </div>
     </div>
